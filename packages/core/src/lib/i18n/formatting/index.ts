d29ec013a11e9a1093b266a5250f85a74e419eb3
Maria Garcia Luque <mfgarcialuque@gmail.com>
2026-05-08 20:13:22 +0200

    [STEP-2.6-007] Add barrel exports for i18n module
    Create formatting/index.ts and i18n/index.ts barrel files, add i18n
    to main src/index.ts. Exposes 13 public symbols: I18nService,
    provideI18n, I18N_CONFIG, FfTranslatePipe, 5 formatting pipes,
    and 4 types (SupportedLocale, LocaleDefinition, I18nConfig, I18nState).

---
+++ packages/core/src/lib/i18n/formatting/index.ts
@@ -0,0 +1,5 @@
+export { FfCurrencyPipe } from './ff-currency.pipe';
+export { FfDatePipe } from './ff-date.pipe';
+export { FfPercentagePipe } from './ff-percentage.pipe';
+export { FfIbanPipe } from './ff-iban.pipe';
+export { FfNifPipe } from './ff-nif.pipe';
